a4ef0c22c870b1a129c3c3c324001e80f1e83dea
Sefa Cihangir <sefa.cihangir@iottech.com.tr>
2022-07-13 08:51:57 +0300

    deadletter exchange example added

--- consumer/src/main/java/com/phoenix/consumer/FileResponseConsumer.java
+++ consumer/src/main/java/com/phoenix/consumer/FileResponseConsumer.java
@@ -1,16 +1,17 @@
 package com.phoenix.consumer;
 
+import com.rabbitmq.client.Channel;
+import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileResponseConsumer {
   private static final Logger LOG = LoggerFactory.getLogger(FileResponseConsumer.class);
-
-  private static final long MAX_FILE_SIZE = 10000;
-
 
 
 
@@ -22,5 +23,17 @@ public class FileResponseConsumer {
       throw new WrongFileException(fileResponse.toString());
     }
   }
+
+  /*
+  @RabbitListener(queues = "file_data.queue", concurrency = "3-7")
+  public void listenQ1(FileResponseModel fileResponse, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag)
+      throws InterruptedException, WrongFileException, IOException {
+    LOG.info("Q1-FileResponse is {}", fileResponse);
+    if ( "csv".equals(fileResponse.getExtension())){
+      channel.basicReject(tag, false);
+      return;
+    }
+    channel.basicAck(tag, false);
+  }*/
 
 }
